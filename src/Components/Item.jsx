efa7cc5c83693a5a4ef354565fafe8e1f1caa318
Guillermo Girau <girauguillermo@gmail.com>
2022-12-15 16:12:58 -0300

    correcion

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Item = ({item}) => {
     return (
         <Link to={"/item/" + item.id} className="text-decoration-none text-dark">
-            <div className="card border border-0 fondoBody">
-                <img src={item.imagen} className="card-img-top" alt={item.nombre} />
+            <div className="card border border-0 text-warning bg-dark mt-2">
+                <img src={item.imagen} width="280" height="280" className="card-img"  alt={item.nombre} />
                 <div className="card-body text-center">
                     <p className="card-text">{item.nombre}</p>
                 </div>
